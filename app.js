901cb7d8bb5333c0b9f99f1f00966047fc2286f7
Hammad Hussain <49366525+Hammabc@users.noreply.github.com>
2021-09-24 23:59:10 +0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Contact from './Contact-form';
+
+function App() {
+  return (
+    <Contact></Contact>
+
+    
+  );
+}
+
+export default App;
